9adb251ca1d0e734729c6f772e2e0ae4a02f2701
aleksey.losev <aleksey.losev@ingate.ru>
2018-08-28 00:20:20 +0300

    Занятие 2

---
+++ guideline/frontend/guideline/dropdown/action.js
@@ -0,0 +1,20 @@
+﻿(function() {
+    let dropdowns = document.querySelectorAll('.dropdown')
+	
+	Array.from(dropdowns).map(function(dropdown) {
+		let content = dropdown.querySelector('.dropdown-content')
+		let btn = dropdown.querySelector('.dropdown-button')
+		let links = content.querySelectorAll('a')		
+				
+		btn.onclick = function() {
+			content.classList.toggle('show')
+		}
+		
+		Array.from(links).map(function(link) {
+			link.onclick = function() {
+				btn.innerHTML = link.innerHTML
+				content.classList.toggle('show')
+			}
+		})
+	})	
+})()
---
+++ guideline/frontend/guideline/modal/action.js
@@ -0,0 +1,74 @@
+﻿(function() {
+    const appendPxAttribute = function(value) {
+		return value + "px"
+	}
+	
+	const openModal = function(modal) {
+		if (modal.className.indexOf('hidden') === -1) {
+			return			
+		}
+		modal.classList.remove('hidden')		
+	}
+	
+	const closeModal = function(modal) {
+		if (modal.className.indexOf('hidden') !== -1) {
+			return			
+		}
+		modal.classList.add('hidden')
+	}
+	
+	window.openModal = openModal
+	window.closeModal = closeModal
+	
+	let btns = document.querySelectorAll('.modal-button')
+
+	Array.from(btns).map(function(btn) {
+		let target = btn.dataset.target
+		let modal = document.getElementById(target)
+		let sizeRaw = modal.dataset.size
+		let contentRaw = modal.dataset.content
+		let backdrop = modal.dataset.backdrop
+		
+		let modalBody = modal.querySelector('.modal-body')
+		let modalBackdrop = modal.querySelector('.modal-backdrop')
+		let modalClose = modalBody.querySelector('.modal-close')
+		let modalHeader = modalBody.querySelector('.modal-header')
+		let modalContent = modalBody.querySelector('.modal-content')		
+		
+		let w = window.innerWidth
+		let h = window.innerHeight
+		
+		let size = JSON.parse(sizeRaw)
+		
+		let wm = w * size.width
+		let hm = h * size.height
+		
+		let left = parseInt((w - wm) / 2)
+		let top = parseInt((h - hm) / 2)
+		
+		modalBody.style.left = appendPxAttribute(left)
+		modalBody.style.top = appendPxAttribute(top)
+		
+		modalBody.style.width = appendPxAttribute(wm)
+		modalBody.style.height = appendPxAttribute(hm)
+		
+		let content = JSON.parse(contentRaw)
+		
+		modalHeader.innerHTML = content.header
+		modalContent.innerHTML = content.content
+		
+		btn.onclick = function() {
+			openModal(modal)
+		}
+		
+		modalClose.onclick = function() {
+			closeModal(modal)
+		}
+		
+		if (backdrop === 'true') {
+			modalBackdrop.onclick = function() {
+				closeModal(modal)
+			}
+		}
+	})	
+})()
